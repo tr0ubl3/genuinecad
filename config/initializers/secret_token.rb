ecde05cea7209b4e19c102ed7d8a362973382919
tr0ubl3 <mentor.1985@gmail.com>
2013-03-22 23:48:11 +0200

    Am creat aplicatia cu bd sqlite pt development

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Genuinecad::Application.config.secret_token = 'dfac5abf1dfacc0855371e2fdb3b2cc529b76e3b7ceb4e096a3840aac746f23f1d6201015ee2e397733a29525da8f5a0b91a73f9ced491d82d663282fff79caa'
